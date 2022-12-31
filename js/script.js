78fd27812b3989673b0910c391fc926026609674
monibasz <monibashafiq123786@gmail.com>
2023-01-01 00:07:49 +0500

    change in js and css

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     
          if($(window).scrollTop()>30){
   
-  $('.header').css({'background':'#ED7014','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+  $('.header').css({'background':'#ED7014'});
          }else{
            $('.header').css({'background':'none','box-shadow':'none'});
          }
